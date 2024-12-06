f328f967f155a95818397554cc75383fb90733aa
Dumitru Botezatu <dima.botezatu@gmail.com>
2024-12-06 19:50:02 +0200

    horizon access to auth user

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -30,9 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return $user !== null;
         });
     }
 }
